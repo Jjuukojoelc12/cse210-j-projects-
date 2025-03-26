d8db442d123fb5c741a72e48a7e1b769b39d2bde
jjuukojoel-mac <jjuukojoel5@gmail.com>
2025-03-26 20:24:31 +0300

    added new code to my program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,8 +2,90 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+   
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
     }
+
+    public Reference GetReference() => _reference;
+    public List<Word> GetWords() => _words;
+    public int GetHiddenWordCount() => _words.Count(w => w.IsHidden());
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+        
+        for (int i = 0; i < numberToHide && visibleWords.Count > 0; i++)
+        {
+            Word word = visibleWords[rand.Next(visibleWords.Count)];
+            word.Hide();
+            visibleWords.Remove(word);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return _reference.GetDisplayText() + " - " + string.Join(" ", _words.Select(w => w.GetDisplayText()));
+    }
+
+    public bool IsCompletelyHidden() => _words.All(w => w.IsHidden());
+}
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide() => _isHidden = true;
+    public void Show() => _isHidden = false;
+    public bool IsHidden() => _isHidden;
+    public string GetText() => _text;
+
+    public string GetDisplayText() => _isHidden ? new string('_', _text.Length) : _text;
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int? _endVerse;
+
+    public Reference(string book, int chapter, int verse, int? endVerse = null)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public string GetBook() => _book;
+    public int GetChapter() => _chapter;
+    public int GetVerse() => _verse;
+    public int? GetEndVerse() => _endVerse;
+
+    public string GetDisplayText()
+    {
+        return _endVerse.HasValue ? $"{_book} {_chapter}:{_verse}-{_endVerse}" : $"{_book} {_chapter}:{_verse}";
+    }
+}
+
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,92 @@
 using System;
+using System.Collections.Generic;
+
+class Comment
+{
+    public string CommenterName { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string commenterName, string text)
+    {
+        CommenterName = commenterName;
+        Text = text;
+    }
+}
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; } // in seconds
+    private List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"  - {comment.CommenterName}: {comment.Text}");
+        }
+        Console.WriteLine(new string('-', 40));
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Creating video instances
+        Video video1 = new Video("C# Basics for Beginners", "Tech Guru", 1200);
+        Video video2 = new Video("Baking a Chocolate Cake", "Cooking World", 900);
+        Video video3 = new Video("Top 10 Travel Destinations", "Globe Trotter", 1500);
+        Video video4 = new Video("Artificial Intelligence Explained", "Tech Insider", 1800);
+
+        // Adding comments to videos
+        video1.AddComment(new Comment("Alice", "Great tutorial!"));
+        video1.AddComment(new Comment("Bob", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Charlie", "Nice explanation."));
+
+        video2.AddComment(new Comment("Dave", "Loved the recipe!"));
+        video2.AddComment(new Comment("Eve", "Tried it, and it's delicious!"));
+        video2.AddComment(new Comment("Frank", "Any vegan alternatives?"));
+
+        video3.AddComment(new Comment("Grace", "I want to visit all these places!"));
+        video3.AddComment(new Comment("Hank", "Amazing recommendations."));
+        video3.AddComment(new Comment("Ivy", "Which one is budget-friendly?"));
+
+        video4.AddComment(new Comment("Jack", "AI is the future!"));
+        video4.AddComment(new Comment("Kate", "Very informative."));
+        video4.AddComment(new Comment("Leo", "Can't wait for the next video!"));
+
+        // Storing videos in a list
+        List<Video> videos = new List<Video> { video1, video2, video3, video4 };
+
+        // Displaying information for each video
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
