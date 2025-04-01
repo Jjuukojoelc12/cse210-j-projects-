a9809386d77003e64d0ddffa47ab2a9cd3b23e1e
jjuukojoel-mac <jjuukojoel5@gmail.com>
2025-04-01 15:38:09 +0300

    new code to my program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,35 @@
-using System;
 
-class Program
-{
-    static void Main(string[] args)
+public class MathAssignment : Assignment
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+    private string _textbookSection = "";
+    private string _problems = "";
+
+    public MathAssignment(string studentName, string topic, string _textbookSection, string problems)
+    :base(studentName, topic)
+    {
+        _textbookSection = _textbookSection;
+        _problems _problems;
     }
+    
+    public  string GetHomeworkList()
+    {
+        return $"section {_textbookSection} Problems {_problems}";
+    }
+    }
+
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+    : base(studentName, topic)
+    {
+        _title = title;
+    } 
+public string GetWritingInformation()
+{
+    string studentName = GetStudentName();
+    return $"title {_title} by {studentName}";
+}
 }
